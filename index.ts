12b1b327a4e372aaa15a3d0fb5f8193122c9c534
Amanshah7705 <20bce259@nirmauni.ac.in>
2024-03-21 14:10:15 +0530

    after changing deploymentt

--- index.ts
+++ index.ts
@@ -2,27 +2,28 @@ import { Server } from "socket.io";
 import express from "express";
 import { createServer } from "http";
 import cors from "cors";
-import { colorAndSize, coordinates } from "./returnType"; 
+import { colorAndSize, coordinates } from "./returnType";
 const app = express();
 app.use(cors());
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {cors: {
-   origin: "https://drawer-frontend.vercel.app/",
-   allowedHeaders: ["my-custom-header"],
-   credentials: true
- }
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://drawer-frontend.vercel.app/",
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
-
 io.on("connection", (socket) => {
-    socket.on('beginPath',(Coordinates:coordinates)=>{
-         socket.broadcast.emit('beginPath',Coordinates)
-    })
-    socket.on('drawPath',(Coordinates:coordinates)=>{
-      socket.broadcast.emit('drawPath',Coordinates)
- })
- socket.on('changeConfig',(colorAndSize:colorAndSize)=>{
-   socket.broadcast.emit('changeConfig',colorAndSize) })
+  socket.on("beginPath", (Coordinates: coordinates) => {
+    socket.broadcast.emit("beginPath", Coordinates);
+  });
+  socket.on("drawPath", (Coordinates: coordinates) => {
+    socket.broadcast.emit("drawPath", Coordinates);
+  });
+  socket.on("changeConfig", (colorAndSize: colorAndSize) => {
+    socket.broadcast.emit("changeConfig", colorAndSize);
+  });
 });
 
 httpServer.listen(5000);
