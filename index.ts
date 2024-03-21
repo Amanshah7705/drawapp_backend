6457daa2d5d1f3ed0227c4e0565ad11dd43e81b2
Amanshah7705 <20bce259@nirmauni.ac.in>
2024-03-21 11:32:28 +0530

    drawapp backend half done

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+import { colorAndSize, coordinates } from "./returnType"; 
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {cors: {
+   origin: "http://localhost:3000",
+   allowedHeaders: ["my-custom-header"],
+   credentials: true
+ }
+});
+
+io.on("connection", (socket) => {
+    socket.on('beginPath',(Coordinates:coordinates)=>{
+         socket.broadcast.emit('beginPath',Coordinates)
+    })
+    socket.on('drawPath',(Coordinates:coordinates)=>{
+      socket.broadcast.emit('drawPath',Coordinates)
+ })
+ socket.on('changeConfig',(colorAndSize:colorAndSize)=>{
+   socket.broadcast.emit('changeConfig',colorAndSize) })
+});
+
+httpServer.listen(5000);
---
+++ returnType.ts
@@ -0,0 +1,10 @@
+export interface coordinates{
+    x:number,
+    y:number
+}
+
+
+export interface colorAndSize{
+    color:string,
+    size:number
+}
